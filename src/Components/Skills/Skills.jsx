c4eb2c2899dc308978c10adabfd13a833140557a
alevexler <alevexler2@gmail.com>
2022-07-01 13:28:30 -0300

    remreplace class with className

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -11,56 +11,56 @@ function Skills() {
         <h3 className={`display-4 text-center fw-bold ${styles.textStyle}`}>Skills</h3>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>HTML</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>CSS</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>JavaScript</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>Bootstrap</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-100 bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>JQuery</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>SASS</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>React JS</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-75 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>MongoDB</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-50 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-50 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
         <div className='d-flex flex-row py-1'>
           <h4 className='col-3 text-end me-2 fs-5 text-light fw-bold'>Express JS</h4>
-          <div class="progress col-7 mt-1">
-            <div class="progress-bar w-50 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          <div className="progress col-7 mt-1">
+            <div className="progress-bar w-50 bg-success" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
       </div>
